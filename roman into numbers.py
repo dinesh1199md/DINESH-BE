74352d16476c60666ad00472383fdc6ac04abbd1
dinesh1199md <51218494+dinesh1199md@users.noreply.github.com>
2019-07-18 16:29:07 +0530

    Update roman into numbers.py

--- roman into numbers.py
+++ roman into numbers.py
@@ -1,1 +1,6 @@
-
+sona=["I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"]
+college=["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"]
+s=input()
+for k in range(20):
+      if(s==sona[k]):
+            print(college[k])
